868d0bb5ffe552915d6ca729e8ecf1fbc4b3adbe
Julie Burk <julieburk@gmail.com>
2017-05-19 11:42:32 -0700

    adding responsive stlye sheet link and metatag

--- test/shoppingListTest.js
+++ test/shoppingListTest.js
@@ -10,19 +10,22 @@ var juliesFunction = function(name){
 
 module.exports = juliesFunction;
 
-// test function
-// app.get("/", function(req, res){
-// 	res.redirect("/listPage");
-// });
 
-// app.get should return data from sql via sequelize orm
+var should = require("chai").should();
+var multiply = function(x, y) {
+  if (typeof x !== "number" || typeof y !== "number") {
+    throw new Error("x or y is not a number.");
+  }
+  else return x * y;
+};
+describe("Multiply", function() {
+  it("should multiply properly when passed numbers", function() {
+    multiply(2, 4).should.equal(8);
+  });
+  it("should throw when not passed numbers", function() {
+    (function() {
+      multiply(2, "4");
+    }).should.throw(Error);
+  });
+});
 
-var getFunction = function(data){
-	if (data === "stuff"){
-		return true
-	}
-	else {
-		return false
-	}
-}
-module.exports = getFunction;
